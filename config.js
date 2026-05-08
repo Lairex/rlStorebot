08584316d2a01f5896f5effd0060aab1ebc990a3
Lairex <90201670+Lairex@users.noreply.github.com>
2026-05-08 20:50:14 +0300

    Add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  STAFF_ROLE_ID: "1464576285945630934",
+  MEMBER_ROLE_ID: process.env.MEMBER_ROLE_ID || "ضع_ID_رتبة_الميمبر",
+  CATEGORY_ID: "1487529737327869963",
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  GUILD_ID: process.env.GUILD_ID || "ضع_ID_السيرفر",
+  CLIENT_ID: process.env.CLIENT_ID || "1487525822184032049",
+  CLIENT_SECRET: process.env.CLIENT_SECRET || "",
+  SESSION_SECRET: process.env.SESSION_SECRET || "lairex-dashboard-secret",
+};
